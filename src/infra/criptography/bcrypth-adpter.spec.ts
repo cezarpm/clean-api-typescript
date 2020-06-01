1d690e3bc562de23c4464c82b4d6c11eb4da13f8
cezar <cezar_pm@hotmail.com>
2020-06-01 17:24:01 -0300

    refactor: remove duplicated test

--- src/infra/criptography/bcrypth-adpter.spec.ts
+++ src/infra/criptography/bcrypth-adpter.spec.ts
@@ -16,7 +16,7 @@ describe('Bcrypt Adapter', () => {
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
   })
 
-  test('Should call bcrypt with correct value', async () => {
+  test('Should return a hash on success', async () => {
     const salt = 12
     const sut = new BcryptAdapter(salt)
     const hash = await sut.encrypt('any_value')
